3721fc726c74b568a19363fcd9f32e71a153309b
Alex Heinen <alexheinen99@gmail.com>
2015-08-15 20:10:26 -0700

    change dirname to actual directory

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,3 +22,4 @@ angular.module('clickInFrontEndApp')
       'Karma'
     ];
   });
+
--- web.js
+++ web.js
@@ -3,5 +3,5 @@ var gzippo = require('gzippo');
   var app = express();
 
   app.use(express.logger('dev'));
-  app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+  app.use(gzippo.staticGzip("" + 'ClickIn-FrontEnd' + "/dist"));
   app.listen(process.env.PORT || 5000);
