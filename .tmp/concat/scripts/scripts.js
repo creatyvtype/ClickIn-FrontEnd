0a43999928c6c141e73ffd6aaf015d94044d868c
Alex Heinen <alexheinen99@gmail.com>
2015-08-16 15:39:45 -0700

    grunt build before deploy

--- .tmp/concat/scripts/scripts.js
+++ .tmp/concat/scripts/scripts.js
@@ -188,17 +188,17 @@ angular.module('clickInFrontEndApp').run(['$templateCache', function($templateCa
   'use strict';
 
   $templateCache.put('views/main.html',
-    "<div class=\"container\"> <h2>Welcome</h2> <div class=\"form-group\"> <h4>Enter a Session Code</h4> <input type=\"text\" ng-model=\"sessionCode\" class=\"form-control\"> <h4>Email</h4> <input type=\"text\" ng-model=\"email\" class=\"form-control\"><br> <div class=\"btn btn-primary\" ng-click=\"update(sessionCode, email)\">Submit</div> </div> </div>"
+    "<div class=\"container\"> <div class=\"form-group\"> <h4>Enter a Code</h4> <input type=\"text\" ng-model=\"sessionCode\" class=\"form-control\" placeholder=\"Session Name\"> <h4>Email</h4> <input type=\"text\" ng-model=\"email\" class=\"form-control\" placeholder=\"Email\"><br> <div class=\"center-btn\"> <a href=\"#/session\" class=\"btn btn-primary\" ng-click=\"update(sessionCode, email)\">Start Session</a> </div> </div> </div>"
   );
 
 
   $templateCache.put('views/results.html',
-    "<h3>{{question.content}}</h3> <h4>Click-Ins: {{question.count}}</h4> <h3>Results</h3> <canvas id=\"pie\" class=\"chart chart-pie\" data=\"data\" labels=\"labels\" legend=\"legend\"></canvas>"
+    "<h3>{{question.content}}</h3> <h4>Click-Ins: {{question.count}}</h4> <h3>Results</h3> <canvas id=\"pie\" class=\"chart chart-pie\" data=\"data\" labels=\"labels\" legend=\"legend\"> </canvas>"
   );
 
 
   $templateCache.put('views/session.html',
-    "<h3>{{question.content}}</h3> <div class=\"row\"> <div ng-repeat=\"answer in answers\" class=\"col-xs-6\"> <div class=\"btn btn-lg\" ng-click=\"clickin(answer)\">{{answer.content}}</div> </div> </div>"
+    "<h3>{{question.content}}</h3> <div class=\"row\"> <div class=\"col s12\"> <div ng-repeat=\"answer in answers\" class=\"answer-wrapper\"> <a href=\"#/results\" class=\"btn btn-lg answer-button\" ng-click=\"clickin(answer)\" ng-touch=\"clickin(answer)\">{{answer.content}}</a> </div> </div> </div>"
   );
 
 }]);
