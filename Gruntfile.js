d238eb5c7d178821d96b6dded747e7ad414aad7b
Alex Heinen <alexheinen99@gmail.com>
2015-08-15 20:20:11 -0700

    user yo heroku npm module

--- Gruntfile.js
+++ Gruntfile.js
@@ -399,6 +399,25 @@ module.exports = function (grunt) {
       ]
     },
 
+    // For heroku deployment
+     copy: {
+        dist: {
+            files: [{
+                expand: true,
+                dest: '<%= yeoman.dist %>',
+                cwd: 'heroku',
+                src: '*',
+                rename: function (dest, src) {
+                    var path = require('path');
+                    if (src === 'distpackage.json') {
+                        return path.join(dest, 'package.json');
+                    }
+                    return path.join(dest, src);
+                }
+            }]
+        }
+    },
+
     // Test settings
     karma: {
       unit: {
