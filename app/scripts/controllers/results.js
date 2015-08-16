7329bd1179c09def7cb7974c6fad53359377efee
Majd Murad <murad.majd@gmail.com>
2015-08-16 14:21:29 -0700

    Updating info through sockets

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -14,10 +14,32 @@ angular.module('clickInFrontEndApp')
     $scope.question = sessionService.question
     $scope.answers = sessionService.answers
 
-    var socket = io("http://clickin-backend.herokuapp.com")
-    socket.on('news', function(data){
-      console.log("RECEIVED: ",data);
+    // $scope.$watch(function(scope){return scope.answers},
+    //   function(newValue, oldValue) {
+    //     $scope.answers = newValue
+    //     newValue.forEach(function(answer))
+    //   }
+    // )
 
+    var setChart = function(answers) {
+      $scope.labels = [];
+      $scope.data = [];
+      $scope.answers = answers;
+      console.log('myanswers', $scope.answers[0].count)
+      answers.forEach(function(answer) {
+        $scope.labels.push(answer.content)
+        $scope.data.push(answer.count)
+      })
+    }
+
+    setChart($scope.answers)
+
+    var socket = io("http://clickin-backend.herokuapp.com")
+    socket.on('result', function(data){
+      console.log("RECEIVED: ",data);
+      $log.log(data.poll.answers)
+      setChart(data.poll.answers)
+      $scope.$apply();
     })
 
     this.awesomeThings = [
